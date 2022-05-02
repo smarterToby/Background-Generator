6035608f3f74ce31159aa68ba29fabc037d2f2f3
Tobias Reuß <tobireuss@gmx.at>
2022-05-02 20:37:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var lodash = require("lodash");
+
+var array = [1, 2,3, 4, 5, 6, 7, 8];
+
+console.log("answer: ", lodash.without(array, 3));
+var css = document.querySelector("#output");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient")
+
+window.onload = setGradient();
+
+function setGradient(){
+  body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+  css.textContent = body.style.background + ";"
+}
+
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
